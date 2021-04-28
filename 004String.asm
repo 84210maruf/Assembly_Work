b2f0a71273a3d2811deefc2b57230596b88b50e6
maruf <maruf84210cse@gmail.com>
2021-04-29 02:23:20 +0600

    check me

--- 004String.asm
+++ 004String.asm
@@ -9,29 +9,29 @@ MAIN PROC
     MOV AX,@DATA    ;INTIALIZATION OF DATA SEGMENT
     MOV DS, AX
     
-    LEA DX, MSG1    ;
-    MOV AH,9
+    LEA DX, MSG1    ;PRINT MSG1
+    MOV AH,9        
     INT 21H
      
      MOV AH,1
-     INT 21H
+     INT 21H        ;INPUT
      MOV BL,AL 
       
      
      MOV AH,2
-     MOV DL, 0DH
+     MOV DL, 0DH    ;NEWLINE
      INT 21H
      MOV DL,0AH
      INT 21H 
       
     
     LEA DX, MSG2
-    MOV AH,9
+    MOV AH,9        ;PRINT MSG2
     INT 21H 
            
      
      MOV AH,2
-     MOV DL, BL
+     MOV DL, BL    ;PRINT BL
      INT 21H
      
      MOV AH, 4CH
